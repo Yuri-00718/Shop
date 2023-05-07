45bcd439085ac2e44fd9e6ed669fe3c5e6878c29
unknown <jlagapito@gbox.adnu.edu.ph>
2023-05-07 13:31:48 +0800

    final changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,9 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-route::put('user/{id}',"HomeController@update")->name('update');
+route::get('/shop',[ProductController::class,'index'])->name('shop');
+Route::get('/shop/add', [ProductController::class, 'create'])->name('add');
+Route::post('/shop/store', [ProductController::class, 'store'])->name('store');
+Route::get('/shop/edit/{id}', [ProductController::class, 'edit'])->name('edit');
+Route::patch('/shop/update/{id}', [ProductController::class, 'update'])->name('update');
+Route::delete('/shop/delete/{id}', [ProductController::class, 'destroy'])->name('delete');
